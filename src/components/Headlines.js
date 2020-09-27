ee9f828302a96e809a4412f45193d89c6ab56f08
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-09-27 11:48:57 -0700

    add middleware and hook up redux store

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -1,55 +1,39 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { makeApiCall } from './../actions/index';
 
 class Headlines extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      headlines: [],
-    };
+    // this.state = {
+    //   error: null,
+    //   isLoaded: false,
+    //   headlines: [],
+    // };
   }
 
-  makeApiCall = () => {
-    fetch(
-      `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((jsonifiedResponse) => {
-        this.setState({
-          isLoaded: true,
-          headlines: jsonifiedResponse.results,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          isLoaded: true,
-          error,
-        });
-      });
-  };
-
   componentDidMount() {
-    this.makeApiCall();
+    const { dispatch } = this.props;
+    dispatch(makeApiCall());
   }
 
   render() {
-    const { error, isLoaded, headlines } = this.state;
+    const { error, isLoading, headlines } = this.props;
     if (error) {
       return <React.Fragment>Error: {error.message}</React.Fragment>;
-    } else if (!isLoaded) {
+    } else if (isLoading) {
       return <React.Fragment>Loading...</React.Fragment>;
     } else {
       return (
         <React.Fragment>
           <h1>Headlines</h1>
           <ul>
-            {headlines.map((headline, index) => (
+            {headlines.map((headline, index) =>
               <li key={index}>
                 <h3>{headline.title}</h3>
                 <p>{headline.abstract}</p>
               </li>
-            ))}
+            )}
           </ul>
         </React.Fragment>
       );
@@ -57,4 +41,12 @@ class Headlines extends React.Component {
   }
 }
 
-export default Headlines;
+const mapStateToProps = state => {
+  return {
+    headlines: state.headlines,
+    isLoading: state.isLoading,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps)(Headlines);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers/headlines-reducer';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import middlewareLogger from './middleware/middleware-logger';
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, middlewareLogger));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
